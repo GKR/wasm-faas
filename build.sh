e3a6c023a9ee5ac2380441a6d9169c1efab726f8
Gunnar Kriik <gunnar.kriik@sparebank1.no>
2023-05-25 10:35:57 +0200

    Added Rust WASM example

--- build.sh
+++ build.sh
@@ -6,13 +6,24 @@ rm /target/*
 # build wasm-faas
 (cd wasm-faas; cargo build --release)
 cp wasm-faas/target/debug/wasm-faas ./target
+cp wasm-faas/target/release/wasm-faas ./target
 
 # build examples
-(cd examples/hello-world-as; npm run build)
+
+# build hello-world-as
+(cd examples/hello-world-as; npm i ; npm run build)
 cp examples/hello-world-as/build/hello-world.wasm ./target
 
-(cd examples/option-pricing-as; npm run build)
+# build hello-world-rs
+(cd examples/hello-world-rs; cargo build --release --target wasm32-wasi)
+cp examples/hello-world-rs/target/wasm32-wasi/release/hello-world-rs.wasm ./target
+
+# build option-pricing-as
+(cd examples/option-pricing-as; npm i ; npm run build)
 cp examples/option-pricing-as/build/option-pricing.wasm ./target
 
+# build sudoku-rs
 (cd examples/sudoku-rs; cargo build --release --target wasm32-wasi)
 cp examples/sudoku-rs/target/wasm32-wasi/release/sudoku-rs.wasm ./target
+
+cp compile-rs.sh ./target
